7b649cb7e5d414d9184ac4635f60ce4326f39202
saitejabaska <122868877+saitejabaska@users.noreply.github.com>
2023-01-23 20:56:05 +0530

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Admin.hasMany(models.Election, {
-        foreignKey: "adminID",
+        foreignKey: "admId",
       });
     }
 
